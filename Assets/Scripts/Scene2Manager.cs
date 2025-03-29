7034c8a55d6eb1e39059c02259f40b99a8f7a2a2
eliott mieze <eliott.mieze@epita.fr>
2025-03-29 20:59:45 +0100

    color

--- Assets/Scripts/Scene2Manager.cs
+++ Assets/Scripts/Scene2Manager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
 public class Scene2Manager : MonoBehaviour
 {
     public ARRaycastManager RaycastManager;
@@ -12,7 +14,9 @@ public class Scene2Manager : MonoBehaviour
     public PlayerInput PlayerInput;
     private InputAction touchPressAction;
     private InputAction touchPosAction;
-    
+    public List<Button> MaterialsButtons;
+    public Image StatusColor;
+    string colorSelected = "red";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,8 +34,57 @@ public class Scene2Manager : MonoBehaviour
         if (hits.Count > 0)
         {
             ARRaycastHit firstHit = hits[0];
-            Instantiate(PrefabToInstantiate, firstHit.pose.position, firstHit.pose.rotation);
+            var obj = Instantiate(PrefabToInstantiate, firstHit.pose.position, firstHit.pose.rotation);
+            var renderer = obj.GetComponent<Renderer>();
+            switch (colorSelected)
+            {
+              case "red":
+                renderer.material.color = Color.red;
+                break;
+              case "green":
+                renderer.material.color = Color.green;
+                break;
+              case "blue":
+                renderer.material.color = Color.blue;
+                break;
+              case "yellow":
+                renderer.material.color = Color.yellow;
+                break;
+              case "pink":
+                renderer.material.color = new Color(1.0f, 0.75f, 0.8f); // Custom pink color
+                break;
+              default:
+                Debug.LogWarning("Unknown color selected: " + colorSelected);
+                break;
+            }
         }
+    }
+    public void ChangeColor(string color) {
+      switch (color) {
+      case "red":
+        colorSelected = "red";
+        StatusColor.color = Color.red;
+        break;
+      case "green":
+        colorSelected = "green";
+        StatusColor.color = Color.green;
+        break;
+      case "blue":
+        colorSelected = "blue";
+        StatusColor.color = Color.blue;
+        break;
+      case "yellow":
+        colorSelected = "yellow";
+        StatusColor.color = Color.yellow;
+        break;
+      case "pink":
+        colorSelected = "pink";
+        StatusColor.color = new Color(1.0f, 0.75f, 0.8f); // Custom pink color
+        break;
+      default:
+        Debug.LogWarning("Unknown color selected: " + color);
+        break;
+      }
     }
 
     void Update()
@@ -41,4 +94,6 @@ public class Scene2Manager : MonoBehaviour
         OnTouch();
       }
     }
+
+    
 }
